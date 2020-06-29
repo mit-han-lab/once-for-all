012dc4716c9f2387d80bce32a6d69967f188a4d8
Ligeng Zhu <ligeng@mit.edu>
2020-06-29 19:03:59 -0400

    [main] add filelock to download_url() to support distributed environments.

--- ofa/utils.py
+++ ofa/utils.py
@@ -174,15 +174,22 @@ class MyNetwork(MyModule):
 def download_url(url, model_dir='~/.torch/', overwrite=False):
     target_dir = url.split('/')[-1]
     model_dir = os.path.expanduser(model_dir)
-    with FileLock(os.path.join(model_dir, "download.lock")) as lock:
-        if not os.path.exists(model_dir):
-            os.makedirs(model_dir)
-        model_dir = os.path.join(model_dir, target_dir)
-        cached_file = model_dir
-        if not os.path.exists(cached_file) or overwrite:
-            sys.stderr.write('Downloading: "{}" to {}\n'.format(url, cached_file))
-            urlretrieve(url, cached_file)
-    return cached_file
+    try:
+        with FileLock(os.path.join(model_dir, "download.lock")) as lock:
+            if not os.path.exists(model_dir):
+                os.makedirs(model_dir)
+            model_dir = os.path.join(model_dir, target_dir)
+            cached_file = model_dir
+            if not os.path.exists(cached_file) or overwrite:
+                sys.stderr.write('Downloading: "{}" to {}\n'.format(url, cached_file))
+                urlretrieve(url, cached_file)
+        return cached_file
+    except Exception as e:
+        # remove lock file so download can be executed next time.
+        os.remove(os.path.join(model_dir, "download.lock"))
+        sys.stderr.write("Failed to download from url %s" % url + "\n" + str(e) + "\n")
+        return None
+
 
 
 def get_same_padding(kernel_size):
