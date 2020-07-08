4e7d827d21c2adc36294b8f308ef8ae3a4730379
Ligeng Zhu <ligeng@mit.edu>
2020-07-08 06:08:02 -0400

    [main] update the download lock.

--- ofa/utils.py
+++ ofa/utils.py
@@ -175,21 +175,20 @@ def download_url(url, model_dir='~/.torch/', overwrite=False):
     target_dir = url.split('/')[-1]
     model_dir = os.path.expanduser(model_dir)
     os.makedirs(model_dir, exist_ok=True)
-    try:
-        with FileLock(os.path.join(model_dir, "download.lock")) as lock:
-            if not os.path.exists(model_dir):
-                os.makedirs(model_dir)
-            model_dir = os.path.join(model_dir, target_dir)
-            cached_file = model_dir
-            if not os.path.exists(cached_file) or overwrite:
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+    model_dir = os.path.join(model_dir, target_dir)
+    cached_file = model_dir
+    if not os.path.exists(cached_file) or overwrite:
+        try:
+            with FileLock(os.path.join(model_dir, "download.lock")) as lock:
                 sys.stderr.write('Downloading: "{}" to {}\n'.format(url, cached_file))
                 urlretrieve(url, cached_file)
-        return cached_file
-    except Exception as e:
-        # remove lock file so download can be executed next time.
-        os.remove(os.path.join(model_dir, "download.lock"))
-        sys.stderr.write("Failed to download from url %s" % url + "\n" + str(e) + "\n")
-        return None
+            return cached_file
+        except Exception as e:
+            os.remove(os.path.join(model_dir, "download.lock")) # remove lock file so download can be executed next time.
+            sys.stderr.write("Failed to download from url %s" % url + "\n" + str(e) + "\n")
+            return None
 
 
 
