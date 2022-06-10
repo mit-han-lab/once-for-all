f572ee50abd095b38a86ca079812e1d7c4f557fd
Ligeng Zhu <ligeng@mit.edu>
2022-06-11 01:29:09 +0800

    update torchhub profiles

--- hubconf.py
+++ hubconf.py
@@ -1,14 +1,14 @@
 dependencies = ['torch', 'torchvision']
 
+from functools import partial
 from ofa.model_zoo import ofa_net, ofa_specialized
 
 # general model
-ofa_resnet50 = ofa_net("ofa_resnet50", pretrained=True)
-ofa_mbv3_w10 = ofa_net("ofa_mbv3_d234_e346_k357_w1.0", pretrained=True)
-ofa_mbv3_w12 = ofa_net("ofa_mbv3_d234_e346_k357_w1.2", pretrained=True)
-ofa_proxyless = ofa_net("ofa_proxyless_d234_e346_k357_w1.3", pretrained=True)
-
+ofa_supernet_resnet50 = partial(ofa_net, net_id="ofa_resnet50", pretrained=True)
+ofa_supernet_mbv3_w10 = partial(ofa_net, net_id="ofa_mbv3_d234_e346_k357_w1.0", pretrained=True)
+ofa_supernet_mbv3_w12 = partial(ofa_net, net_id="ofa_mbv3_d234_e346_k357_w1.2", pretrained=True)
+ofa_supernet_proxyless = partial(ofa_net, net_id="ofa_proxyless_d234_e346_k357_w1.3", pretrained=True)
 
 # specialized
-resnet50d_mac_06b = ofa_specialized("resnet50D_MAC@0.6B_top1@75.0_finetune@25", pretrained=True)
+resnet50d_mac_06b = partial(ofa_specialized, net_id="resnet50D_MAC@0.6B_top1@75.0_finetune@25", pretrained=True)
 
